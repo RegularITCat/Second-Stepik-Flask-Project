ca538c0795b797f482e6f0ca4942e78c07df63ce
RegularITCat <regularitcat@gmail.com>
2020-05-24 23:11:39 +0700

    third week fixes

--- flask/views.py
+++ flask/views.py
@@ -1,9 +1,9 @@
 import json
 import random
-from flask import render_template, request, redirect, url_for
+from flask import render_template
 from app import app
 from forms import *
-
+import pprint
 
 with open("data.json") as f:
     data = json.loads(f.read())
@@ -19,26 +19,32 @@ def index():
 
 @app.route("/goals/<goal>/")
 def goal(goal):
-    new_data = {
-        "teachers": [],
-        "goal": data["goals"].get(goal).lower(),
-        "goal_emoji": data["goals_emoji"][goal]
-    }
-    for e in data["teachers"]:
-        if goal in e["goals"]:
-            new_data["teachers"].append(e)
-    return render_template("goal.html", **new_data)
+    if goal in data['goals'].keys():
+        new_data = {
+            "teachers": [],
+            "goal": data["goals"].get(goal).lower(),
+            "goal_emoji": data["goals_emoji"][goal]
+        }
+        for e in data["teachers"]:
+            if goal in e["goals"]:
+                new_data["teachers"].append(e)
+        return render_template("goal.html", **new_data)
+    else:
+        return "err", 404
 
 
 @app.route("/profiles/<int:teacher_id>/")
 def profile(teacher_id):
-    for e in data["teachers"]:
-        if e["id"] == teacher_id:
-            teacher = e
-            break
-    new_data = {"goals": data["goals"], "teacher": teacher, "days": data[
-        "days"], "goals_emoji": data["goals_emoji"]}
-    return render_template("profile.html", **new_data)
+    if teacher_id in [d['id'] for d in data['teachers']]:
+        for e in data["teachers"]:
+            if e["id"] == teacher_id:
+                teacher = e
+                break
+        new_data = {"goals": data["goals"], "teacher": teacher, "days": data[
+            "days"], "goals_emoji": data["goals_emoji"]}
+        return render_template("profile.html", **new_data)
+    else:
+        return "err", 404
 
 
 @app.route("/request/", methods=["GET", "POST"])
@@ -64,19 +70,25 @@ def request_done():
             f.write(json.dumps(request_data))
         return render_template("request_done.html", **new_data, goals=data["goals"])
     else:
-        return "errs"
+        return "err", 404
 
 
-@app.route("/booking/<int:teacher_id>/<day>/<int:time>/", methods=["GET", "POST"])
+@app.route("/booking/<int:teacher_id>/<day>/<time>/", methods=["GET", "POST"])
 def booking(teacher_id, day, time):
-    for e in data["teachers"]:
-        if e["id"] == teacher_id:
-            teacher = e
-            break
-    new_data = {"goals": data["goals"], "teacher": teacher, "day": day, "time": str(
-        time) + ":00", "days": data["days"]}
-    form = BookingForm()
-    return render_template("booking.html", form=form, **new_data)
+    if (teacher_id in [d['id'] for d in data['teachers']]) and (day in data["days"].keys()) and (int(time) in range(0, 24, 2)):
+        time = time + ":00"
+        for e in data["teachers"]:
+            if e["id"] == teacher_id:
+                teacher = e
+                break
+        if teacher.get('free').get(day).get(time):
+            new_data = {"goals": data[
+                "goals"], "teacher": teacher, "day": day, "time": time, "days": data["days"]}
+            form = BookingForm()
+            return render_template("booking.html", form=form, **new_data)
+        return "err", 404
+    else:
+        return "err", 404
 
 
 @app.route("/booking_done/", methods=["GET", "POST"])
@@ -95,7 +107,6 @@ def booking_done():
             "time": time,
             "teacher": teacher
         }
-        # print(data["days"].get(request.form["weekday"]))
         for e in data['teachers']:
             if e["id"] == teacher:
                 ((e['free'])[weekday])[time] = False
